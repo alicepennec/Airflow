8e519c56d04d7f69c57d116d25bb7fa6cc775030
alicepennec <alicepennec@gmail.com>
2026-01-05 11:19:56 +0100

    cron set to none

--- dags/JO_dag.py
+++ dags/JO_dag.py
@@ -120,7 +120,7 @@ def load_data(**kwargs):
 dag = DAG(
     'etl_pipeline_JO',
     description             = 'Pipeline ETL & Data Control avec Soda',
-    #schedule_interval       = '0 * * * *',
+    schedule_interval       = '',
     start_date              = datetime(2025, 6, 2),
     catchup                 = False,
     is_paused_upon_creation = False 
