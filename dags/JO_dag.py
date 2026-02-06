4564fece844ffef8147b8b7e6c3903a91ebab7ec
alicepennec <alicepennec@gmail.com>
2026-02-06 17:16:40 +0100

    changement frequence logs

--- dags/JO_dag.py
+++ dags/JO_dag.py
@@ -114,7 +114,7 @@ def load_data(**kwargs):
 dag = DAG(
     'etl_pipeline_JO',
     description             = 'Pipeline ETL & Data Control avec Soda',
-    schedule_interval       = '@hourly',
+    schedule_interval       = '@daily',
     start_date              = datetime(2025, 6, 2),
     catchup                 = False,
     is_paused_upon_creation = False 
